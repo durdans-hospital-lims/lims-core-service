2cb0040c3ccf7487e82d4fce21924b270cd5b9f3
Lasith Undulanga <ludulanga@gmail.com>
2026-04-25 13:48:15 +0530

    Resolve merge conflicts with develop

--- lims-core-service-app/src/main/java/com/uom/lims/repository/TestResultRepository.java
+++ lims-core-service-app/src/main/java/com/uom/lims/repository/TestResultRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TestResultRepository extends JpaRepository<TestResultEntity, UUID> {
 
     Page<TestResultEntity> findByStatus(ResultStatus status, Pageable pageable);
+
+    List<TestResultEntity> findBySampleId(UUID sampleId);
+
+    Optional<TestResultEntity> findBySampleIdAndParameterId(UUID sampleId, UUID parameterId);
 }
