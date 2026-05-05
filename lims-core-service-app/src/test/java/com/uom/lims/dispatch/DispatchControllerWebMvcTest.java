5c708e7c4452057fa01a8a1dac3b4d1deed9878a
kalanas210 <kalanasandakelum210@gmail.com>
2026-05-05 23:18:55 +0530

    gradle configuration update

--- lims-core-service-app/src/test/java/com/uom/lims/dispatch/DispatchControllerWebMvcTest.java
+++ lims-core-service-app/src/test/java/com/uom/lims/dispatch/DispatchControllerWebMvcTest.java
@@ -4,9 +4,9 @@ import com.uom.lims.api.common.PageResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -24,7 +24,7 @@ class DispatchControllerWebMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DispatchService dispatchService;
 
     @Test
