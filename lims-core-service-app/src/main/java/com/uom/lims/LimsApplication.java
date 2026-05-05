d258104497c142a8e24430910a0a75f4d6a92567
kalanas210 <kalanasandakelum210@gmail.com>
2026-05-05 23:18:55 +0530

    gradle configuration update

--- lims-core-service-app/src/main/java/com/uom/lims/LimsApplication.java
+++ lims-core-service-app/src/main/java/com/uom/lims/LimsApplication.java
@@ -3,11 +3,9 @@ package com.uom.lims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableCaching
-@EnableJpaAuditing
 @EnableAsync
 @SpringBootApplication
 public class LimsApplication {
